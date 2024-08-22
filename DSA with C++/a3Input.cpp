071af0bb8c436845d1ac11ff3089b24ec09a44be
Rohan Sanjay Pol <120255126+9307632368@users.noreply.github.com>
2024-08-22 19:16:49 +0530

    Add files via upload

---
+++ DSA with C++/a3Input.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    //how to take input in c++
+    int a,b,ans;
+    cout<<"Enter a first number\n";
+    cin>>a;
+    cout<<"Enter a second number\n";
+    cin>>b;
+    ans=a+b;
+    cout<<ans;
+
+}
---
+++ DSA with C++/typeCasting.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    // type casting
+    int a=10;
+    char x='A';
+
+    a=x;
+    cout<<a<<endl;
+}

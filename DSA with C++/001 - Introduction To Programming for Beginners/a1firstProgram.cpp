3cd19868d23318386d46d7647b98fccd6d8340a9
Rohan Sanjay Pol <120255126+9307632368@users.noreply.github.com>
2024-08-23 22:34:38 +0530

    Add files via upload

---
+++ DSA with C++/001 - Introduction To Programming for Beginners/a1firstProgram.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    std::cout<<"this is rohan pol\n";
+    cout<<"what are you doing"<<endl;
+    cout<<3+5;
+};
---
+++ DSA with C++/001 - Introduction To Programming for Beginners/typeCasting.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+int main(){
+    // type casting
+    int a=10;
+    char x='A';
+
+    a=x;
+    cout<<a<<endl;
+
+    int b=80;
+    char y='c';
+
+    y=b;
+    cout<<y<<endl;
+
+    //typecasting int to long int
+    int shortNumber=10;
+    long int largeNumber;
+
+    largeNumber=shortNumber;
+    cout<<largeNumber<<endl;
+
+   //typecasting long int to int
+    long long int longNumber= 9307632368;
+    cout<<longNumber<<endl;
+    int smallNumber;
+    smallNumber=longNumber;
+    cout<<smallNumber<<endl;
+
+   
+}
